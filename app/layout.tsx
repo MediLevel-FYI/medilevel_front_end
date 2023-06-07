ae2a029ffc185a9cdd069f8da824e3c78db8d082
Harper Foley <harper.e.foley@gmail.com>
2023-06-07 16:17:10 -0700

    Adds initial components framework. - Header - Footer - Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'MediLevels.FYI',
+  title: 'MediLevels.fyi',
   description: 'Discover comprehensive compensation data for healthcare providers. Compare salaries, bonuses, and stock compensation based on specialty, years of experience, location, and more. Make informed decisions about your career in healthcare.',
 }
 
@@ -15,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center min-h-screen p-24">
       <h1>Hello, World!</h1>
+      <p>Main body area for charts, graphs, etc.</p>
     </main>
   )
 }
