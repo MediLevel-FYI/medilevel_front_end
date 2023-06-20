ea9fdaf4cc0c0d0e51250a43b672fcbec4e82cde
Harper Foley <harper.e.foley@gmail.com>
2023-06-20 12:10:27 -0700

    Finishes About section.
    - Creates AboutUs component for general info about the project
    - Moves FAQ data and dummy compensation data to new `_data` folder to clean up components

---
+++ components/AboutUs.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className="max-w-2xl mx-auto">
+
+      <ul className="mt-4">
+        <blockquote className="italic bg-gray-200 text-gray-800 p-2 rounded-sm my-2 dark:text-gray-200">
+          “When I was applying for jobs out of residency, I had to figure out whether anyone I knew was at a practice, then I had to see if they would be willing to provide me any information about how they&apos;re paid. Sharing this information is done so ad-hoc and informally - it&apos;s so hard to understand what my best options are, as far as pay, until I invest a ton of time and hopefully receive an offer.”
+        </blockquote>
+        <blockquote className="italic bg-gray-200 text-gray-800 p-2 rounded-sm my-2 dark:text-gray-200">
+          “Looking at an offer from a hospital, I have no idea whether I&apos;m being paid more, less, or the same as my future colleagues. I always get the sense that they&apos;re trying to pay me as little as they need to - I wish the process was more transparent and clear.”
+        </blockquote>
+        <blockquote className="italic bg-gray-200 text-gray-800 p-2 rounded-sm my-2 dark:text-gray-200">
+          “During COVID, I found out through conversations that new hires were being paid substantially more than the rest of the team who have been here for years, if not over a decade. Admin never felt like they had to address this - but we all talked about it behind the scenes.”
+        </blockquote>
+      </ul>
+
+      <div className="mt-5">
+        <p className="my-2 text-gray-800 dark:text-gray-200">
+          At MedPay, we understand the transformative potential of information sharing on boosting compensation for workers. We&apos;ve witnessed the power of organized knowledge exchange to transform technology, professional services, and other industry practices firsthand. Now, we aim to extend this impact to the medical field.
+        </p>
+        <p className="my-2 text-gray-800 dark:text-gray-200">
+          Our mission is to create a platform that empowers medical workers by facilitating transparency, allowing discussions for fair wages. By fostering a culture of transparency, organization, and community, we aspire to revolutionize compensation practices for medical providers. Join us on this journey toward a future where wage transparency and collective empowerment redefine possibilities for all medical professionals.
+        </p>
+      </div>
+    </div>
+
+  )
+}
+
+export default AboutUs
