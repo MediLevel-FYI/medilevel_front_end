9f9dc38b64553b364b9937760f6203ebe38ec645
Harper Foley <harper.e.foley@gmail.com>
2023-07-06 13:23:42 -0700

    Creates new FAQ page. Moves Faq component to FAQ page. Reorders FAQs. Adds FAQs to header via NavLinks.

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import Faq from '@/components/Faq'
 import { faqs } from '@/_data/_faq'
-import AboutUs from '@/components/AboutUs'
 
-
-const About = () => {
+const faqPage = () => {
   return (
-
     <div className="max-w-[85rem] px-4 py-5 sm:px-6 lg:px-8 lg:py-7 mx-auto">
       <div className="max-w-2xl mx-auto mb-5 text-center lg:mb-14">
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">About</h2>
+        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">FAQ</h2>
+        <p className="mt-1 text-gray-600 dark:text-gray-400">Answers to the most frequently asked questions.</p>
       </div>
-      <AboutUs />
+      <Faq faqs={faqs} />
     </div>
-
   )
 }
 
-export default About
+export default faqPage
--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -7,19 +7,28 @@ import ContributeModal from './ContributeModal'
 const NavLinks = () => {
   const isHomeActive = useActiveLink('/')
   const isAboutActive = useActiveLink('/about')
+  const isFaqActive = useActiveLink('/faq')
 
   return (
     <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
       <Link 
-        className={`font-medium ${isHomeActive ? 'text-blue-500' : 'text-gray-600'} hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500`} href="/" 
+        className={`font-medium ${isHomeActive ? 'text-blue-500' : 'text-gray-600'} hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500`} 
+        href="/" 
         aria-current="page"
       >
         Home
       </Link>
       <Link 
-        className={`font-medium ${isAboutActive ? 'text-blue-500' : 'text-gray-600'} hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500`} href="/about"
+        className={`font-medium ${isAboutActive ? 'text-blue-500' : 'text-gray-600'} hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500`} 
+        href="/about"
       >
         About
+      </Link>
+      <Link 
+        className={`font-medium ${isFaqActive ? 'text-blue-500' : 'text-gray-600'} hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500`}
+        href="/faq"
+      >
+        FAQ
       </Link>
       <ContributeModal />
       {/* <Link className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Salaries</Link> */}
