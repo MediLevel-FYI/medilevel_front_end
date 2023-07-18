684e1ff541fae6d4a081adda2a03b706ecfb1011
Harper Foley <harper.e.foley@gmail.com>
2023-07-18 15:49:17 -0700

    Minor fixes on modal and formatting.

--- _data/_faq.ts
+++ _data/_faq.ts
@@ -1,13 +1,5 @@
 export const faqs = [
   {
-    "question": "Who are we?",
-    "answer": "We are two technology professionals in Seattle who, through our families and friends, are intimately familiar with the professional culture and compensation practices for providers in the healthcare industry. We have long observed a sharp contrast between the labor practices of healthcare and technology. While technology also has its own struggles and improvement roadmap, we perceive healthcare and hospitals as decades behind modern HR thinking and practice. This project is aimed at providing transparency and data to make the case for broader change."
-  },
-  {
-    "question": "What will you do with this information?",
-    "answer": "It’s simple: we are aggregating your data and reflecting it back to users of this website to the fullest extent we can while protecting the identities of submitters. We will never sell your data to 3rd parties. Not much more to it than that."
-  },
-  {
     "question": "How can I use this information?",
     "answer": [
       "Benchmark & Baseline: Understand whether your current offer is comparable to your market value and what your peers are being paid.",
@@ -16,7 +8,15 @@ export const faqs = [
     ]
   },
   {
+    "question": "What will you do with this information?",
+    "answer": "It’s simple: we are aggregating your data and reflecting it back to users of this website to the fullest extent we can while protecting the identities of submitters. We will never sell your data to 3rd parties. Not much more to it than that."
+  },
+  {
     "question": "Is this legal?",
     "answer": "Discussing compensation is not only essential but also protected by law. The National Labor Relations Board (NLRB) safeguards your rights to engage in open conversations about wages, benefits, and working conditions, regardless of whether you are part of a union or not. You have the freedom to share and gather information to promote fairness and transparency in the workplace. To learn more about your rights and the legal protection surrounding discussions of wages, we encourage you to visit the NLRB's informative page: https://www.nlrb.gov/about-nlrb/rights-we-protect/your-rights/your-rights-to-discuss-wages Remember, knowledge is power, and together, we can advocate for fair compensation practices."
+  },
+  {
+    "question": "Who are you?",
+    "answer": "We are two technology professionals in Seattle who, through our families and friends, are intimately familiar with the professional culture and compensation practices for providers in the healthcare industry. We have long observed a sharp contrast between the labor practices of healthcare and technology. While technology also has its own struggles and improvement roadmap, we perceive healthcare and hospitals as decades behind modern HR thinking and practice. This project is aimed at providing transparency and data to make the case for broader change."
   },
 ]
