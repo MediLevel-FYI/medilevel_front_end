59925e05cb96ca334f88839b11692e20d097d85a
Harper Foley <harper.e.foley@gmail.com>
2023-07-20 20:25:14 -0700

    Minor fixes to formatting and menu toggle

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <footer className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
 
       <div className="text-center">
         <div>
-          <Link className="flex-none text-xl font-semibold text-black dark:text-white" href="/" aria-label="Brand">MedPay</Link>
+          <Link 
+            className="flex-none text-xl font-semibold text-black dark:text-white" href="/"
+            aria-label="Brand"
+          >
+            MedPay
+          </Link>
         </div>
   
 
         <div className="mt-3">
           <p className="text-gray-500">Level up your medical career with <span className="font-semibold text-black underline">accurate</span> salary data.</p>
-          <p className="text-gray-500">© MedPay. 2023 &middot; All Rights Reserved.</p>
+          <p className="text-gray-500">
+            © MedPay. 2023 &middot; All Rights Reserved.
+          </p>
         </div>
 
   
